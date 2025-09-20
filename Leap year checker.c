f36070803e585905bc7d61afc9ce7305c10a05be
Faizan khan <fk342219@gmail.com>
2025-09-20 08:37:43 -0700

    Add files via upload

---
+++ Leap year checker.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	int year;
+	//Input year
+	printf("Enter year: ");
+	scanf("%d",&year);
+	
+	//Check leap year
+	if((year%400==0) || (year%4==0 && year%100!=0))                      
+	 printf("The year is a leap year");
+	 
+	 else
+	 printf("The year is not a leap year");
+	return 0;
+}
