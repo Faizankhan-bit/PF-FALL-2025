6712cb55c825d42932e2d7bee1c0359c2cb79cd6
Faizan khan <fk342219@gmail.com>
2025-11-03 07:06:13 -0800

    lab 9
    Implement a simple C program demonstrating constant pointers.

---
+++ PF Lab/lab-9/Task 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main( ){
+ int y = 5,z=6;
+ int const *yPtr = &y;
+ printf("%p\n", yPtr);
+// *yPtr = &z;    This line gives an error because we are reassigning an adress to a constant pointer which is not allowed
+// printf("%p\n", yPtr);
+
+return 0;
+}
