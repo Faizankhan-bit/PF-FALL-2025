b5ae43f5aa202a4da6c38808dc72074427777dda
Faizan khan <fk342219@gmail.com>
2025-11-10 05:50:04 -0800

    Add files via upload

---
+++ Practice/Reverse Array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+   void printArray(int a[], int n){
+      int i;
+   	for(i=0; i<n; i++ ){
+   		printf("%d ", a[i]);
+	   }
+	   printf("\n");
+	   
+   }
+   void reverse(int arr[], int n){
+   
+   int temp,i;
+   for(i=0; i<n/2; i++ ){
+   	temp=arr[i];
+   	arr[i]=arr[n-i-1];
+   	arr[n-i-1]=arr[i];
+   }
+}
+int main(){
+	int arr[]={1,2,3,4,5,6};
+	printArray(arr, 6);
+	reverse(arr, 6);
+    printArray(arr, 6);
+	 
+	 return 0;
+}
